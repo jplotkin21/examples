32fc8690eeb49fcd3b0183984e328fb9855e4d4f
josephplotkin <jplotkin21@gmail.com>
2019-04-05 17:37:24 +0800

    correctly recovering sample covar, correl matrices

--- example_pca.py
+++ example_pca.py
@@ -4,9 +4,11 @@
 import numpy as np
 import argparse
 import logging
-import pdb
+
 
 logging.basicConfig(level=logging.INFO)
+
+np.set_printoptions(precision=3)
 
 
 def _parse_args():
@@ -14,7 +16,8 @@ def _parse_args():
     parser = argparse.ArgumentParser(description=_description)
     parser.add_argument('-o', '--observations', type=int, default=100, help='number of observations')
     parser.add_argument('-c','--correlation', type=float, default=0.5, help='correlation between the two variables')
-    parser.add_argument('-v', '--volatility', type=list, default=[0.2, 0.2], help='volatility of the two variables')
+    parser.add_argument('-v', '--volatility', nargs=2, type=float, default=[0.2, 0.2],
+                        help='volatility of the two variables')
 
     args = parser.parse_args()
 
@@ -24,19 +27,24 @@ def _parse_args():
 def _compute_sample_vector(observations, volatility_vector, correlation):
     std_norm_vector = np.random.randn(observations, 2)
     correlated_array = std_norm_vector[:, 0] * correlation + np.sqrt(1 - correlation**2) * std_norm_vector[:, 1]
-    correlated_std_norm = list(zip(std_norm_vector[:, 0], correlated_array))
-    res = correlated_std_norm * volatility_vector
+    correlated_std_norm = np.column_stack((std_norm_vector[:, 0], correlated_array))
+    res = np.multiply(correlated_std_norm, volatility_vector)
     return res
 
 
 def main():
     args = _parse_args()
-
+    corr = args.correlation
+    vol_array = args.volatility
+    logging.info('volatility vector: {}, correlation: {}'.format(vol_array, corr))
+    theoretical_covariance = np.matrix([[vol_array[0]**2, vol_array[0]*vol_array[1]*corr],
+                                       [vol_array[0]*vol_array[1]*corr, vol_array[1]**2]])
+    logging.info('theoretical covariance:\n{}'.format(theoretical_covariance))
     correlated_rvs = _compute_sample_vector(args.observations, args.volatility, args.correlation)
     sample_covariance = np.cov(correlated_rvs, rowvar=False)
     sample_correlation = np.corrcoef(correlated_rvs, rowvar=False)
-    logging.info(sample_covariance)
-    logging.info(sample_correlation)
+    logging.info('sample covariance:\n{}'.format(sample_covariance))
+    logging.info('sample correlation:\n{}'.format(sample_correlation))
 
 
 if __name__ == '__main__':
